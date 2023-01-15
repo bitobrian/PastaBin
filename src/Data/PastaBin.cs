122a8dc2ba4c77ee7a4da7c544aac30327e722aa
Brian Donaldson <brian@bitobrian.com>
2023-01-15 13:03:19 -0600

    init

---
+++ src/Data/PastaBin.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Blazorbin.Data;
+
+public class PastaBin
+{
+    [BsonId(IdGenerator = typeof(CombGuidGenerator))]
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
---
+++ src/Services/DbService.cs
@@ -0,0 +1,44 @@
+﻿using Blazorbin.Data;
+using MongoDB.Driver;
+
+namespace Blazorbin.Services;
+
+public class DbService
+{
+    private readonly IMongoDatabase _db;
+    private readonly IMongoCollection<PastaBin> _pastaBins;
+    private readonly MongoClient _client;
+    
+    public DbService()
+    {
+        _client = new MongoClient("mongodb://localhost:27017");
+        _db = _client.GetDatabase("Pasta");
+        _pastaBins = _db.GetCollection<PastaBin>("PastaBins");
+    }
+    
+    public async Task<PastaBin> GetPasta(string id)
+    {
+        var filter = Builders<PastaBin>.Filter.Eq("Id", id);
+        var result = await _pastaBins.FindAsync(filter);
+        return result.FirstOrDefault();
+    }
+    
+    public async Task<PastaBin> CreatePasta(PastaBin pastaBin)
+    {
+        await _pastaBins.InsertOneAsync(pastaBin);
+        return pastaBin;
+    }
+    
+    public async Task<PastaBin> UpdatePasta(PastaBin pastaBin)
+    {
+        var filter = Builders<PastaBin>.Filter.Eq("Id", pastaBin.Id);
+        await _pastaBins.ReplaceOneAsync(filter, pastaBin);
+        return pastaBin;
+    }
+    
+    public async Task DeletePasta(string id)
+    {
+        var filter = Builders<PastaBin>.Filter.Eq("Id", id);
+        await _pastaBins.DeleteOneAsync(filter);
+    }
+}
